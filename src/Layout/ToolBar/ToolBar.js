4547d7e736ffaca719d3f83b41ffc81649a36206
Mario <marina.seider@gmail.com>
2019-07-12 14:04:40 +0200

    Test

--- src/Layout/ToolBar/ToolBar.js
+++ src/Layout/ToolBar/ToolBar.js
@@ -4,10 +4,12 @@ import ToolBarBtn from './ToolBarBtn/ToolBarBtn';
 
 const ToolBar = () => {
     return (
-        <div>
+        <div className='toolBar'>
             <ToolBarBtn type="add">Add</ToolBarBtn>
             <ToolBarBtn type="my_profile">My profile</ToolBarBtn>
             <ToolBarBtn type="sfjdskfjd">djdskjfskdjfd</ToolBarBtn>
+            <ToolBarBtn type="show">Show</ToolBarBtn>
+
         </div>
     );
 }
