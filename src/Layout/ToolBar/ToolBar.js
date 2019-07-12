7da7c2306b0af95c7c82d2484cd4afa16bf4ee2f
Maja <majamilas2@gmail.com>
2019-07-12 13:25:53 +0200

    ToolBar Edited

--- src/Layout/ToolBar/ToolBar.js
+++ src/Layout/ToolBar/ToolBar.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './ToolBar.css';
+import ToolBarBtn from './ToolBarBtn/ToolBarBtn';
 
 const ToolBar = () => {
     return (
-        <div className="box1"> shdfhsdjfhdsj
+        <div>
+            {/*Nemoj dirat typeove*/}
+            <ToolBarBtn type='add' icon="fsdjfdsk">Add</ToolBarBtn>
+            <ToolBarBtn type='my_profile'>My Profile</ToolBarBtn>
+            <ToolBarBtn type='dgdfk'>Nesto trece</ToolBarBtn>
         </div>
     );
 }
