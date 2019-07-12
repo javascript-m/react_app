c2050bc4625df1cf0808f072d83a8c5b8a48b2f4
Maja <majamilas2@gmail.com>
2019-07-12 10:48:45 +0200

    Basic component tree

---
+++ src/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Layout = () => {
+    return (
+        <div>
+            {/*Komponente koje treba napraviti: */}
+            <div>ToolBar</div>
+            <div>Sidebar</div>
+        </div>
+    );
+}
+
+export default Layout;
---
+++ src/Pages/FoodTracker/FoodTracker.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class FoodTracker extends Component {
+    render() {
+        return (
+            <div>
+                <div>Add Meal Comp</div>
+                <div>Stats Comp</div>
+            </div>
+        );
+    }
+}
