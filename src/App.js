40460f632a521f4d67238d6aab0efe3605e61a9d
Maja <majamilas2@gmail.com>
2019-07-12 15:24:46 +0200

    Commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
 import Layout from './Layout/Layout';
-import FoodTracker from './Pages/FoodTracker/FoodTracker';
 
 const App = () => {
   return (
     <div className="App">
       <Layout></Layout>
-      <FoodTracker></FoodTracker>
     </div>
   );
 }
--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import SideBar from './SideBar/SideBar';
 import ToolBar from './ToolBar/ToolBar';
+import FoodTracker from '../Pages/FoodTracker/FoodTracker';
+
+import './Layout.css';
 
 const Layout = () => {
     return (
         <div>
             <ToolBar></ToolBar>
-            <SideBar></SideBar>
+           <div>
+                <SideBar></SideBar>
+                <div className='page'>
+                    <FoodTracker></FoodTracker>
+                </div>
+           </div>
         </div>
     );
 }
